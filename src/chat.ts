9fe1d525bc4d8e401b5c3234fe120e3e262c6b1f
Peng Wang <pengxlyw@gmail.com>
2023-03-12 16:56:07 +0800

    add more detailed requirement to prompt

--- src/chat.ts
+++ src/chat.ts
@@ -9,7 +9,18 @@ export class Chat {
   }
 
   private generatePrompt = (patch: string) => {
-    return `Bellow is the code patch, please help me do a brief code review, if any bug risk and improvement suggestion are welcome
+    return `Act as a coding reviewer of a open source project on github that makes sure the code to commit is well-designed, easy to understand,  bug-free(such as no data race, no memory leak, no dead-lock, no invalid memory access, and so on), consistent with the existing code.
+
+    One important thing to note: the time of developers is scarce, and they care more about negative feedbacks (such bugs) or improvements, so positive feedback can be ignored or just a simple response, such as 'LGTM', which is an acronym for ‘Looks Good To Me’; and try to keep the total feedback as shorten as possible.
+
+    when the code  patch is given to you, you will review:
+    1. whether the changes from the patch are well designed?
+    2. whether the changes have any bugs or risk? if so, give the details;
+    3. is there any more more elegance or simpler way to achieve the same effect? if there is, give it;
+    4. is there any improvement suggestion? if so , give it.
+    5. whether the code style is consistent? if not, pinpoint it.
+
+    Below is the patch to review:
     ${patch}
     `;
   };
